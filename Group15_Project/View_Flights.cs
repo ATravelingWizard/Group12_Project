633729ba7911ef38d21051ff7c794d8ad0cd0fef
ATravelingWizard <henardgertzen@gmail.com>
2022-09-05 13:03:37 +0200

    Added new forms

---
+++ Group15_Project/View_Flights.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group15_Project
+{
+    public partial class View_Flights : Form
+    {
+        public View_Flights()
+        {
+            InitializeComponent();
+        }
+    }
+}
